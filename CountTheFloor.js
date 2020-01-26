b1a29f5d20cdc85017522ddacd9fdb196d5b6365
Tatiana Smolin <tatismolin@gmail.com>
2020-01-26 16:37:27 -0700

    Solved CountTheFloor kata.

---
+++ CountTheFloor.js
@@ -0,0 +1,22 @@
+// Americans are odd people: in their buildings, the first floor is actually the ground floor and there is no 13th floor ('cause of superstition).
+
+// Write a function that given an American floor (passed as an integer) returns the real floor.
+// Moreover, your function should work for basement floors too: just return the same value as the passed one.
+
+// Examples:
+// getRealFloor(1) == 0 
+// getRealFloor(0) == 0 // Special case to please Europeans
+// getRealFloor(5) == 4
+// getRealFloor(15) == 13
+// getRealFloor(-3) == -3
+
+function getRealFloor(n){
+    if(n >= 1 && n < 13){
+        return n - 1;
+    }else if(n >= 13){
+        return n - 2;
+    }else{
+      return n;
+    }
+  };
+
