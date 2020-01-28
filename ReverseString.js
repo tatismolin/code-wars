7f4fd227a16f98ae11d0ae4212c51eefe6c94174
Tatiana Smolin <tatismolin@gmail.com>
2020-01-28 01:09:19 -0700

    Solved ReverseString kata.

---
+++ ReverseString.js
@@ -0,0 +1,13 @@
+// Complete the solution so that it reverses the string value passed into it.
+
+// Examples:
+// solution('world'); // returns 'dlrow'
+
+function solution(str){
+    let newStr = "";
+  
+    for(let i = str.length-1; i >= 0; i--){
+      newStr += str[i]
+    }
+    return newStr;
+  };
