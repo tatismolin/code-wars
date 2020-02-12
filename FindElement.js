f8e547c7521ff65ba6aa217c05bbe9243ea33412
Tatiana Smolin <tatismolin@gmail.com>
2020-02-11 23:13:43 -0700

    Solved FindElement kata.

---
+++ FindElement.js
@@ -0,0 +1,10 @@
+// You will be given an array (a) and a value (x). All you need to do is check whether the provided array contains the value, without using a loop.
+// Array can contain numbers or strings. X can be either. Return true if the array contains the value, false if not. With strings you will need to account for case.
+
+function check(a,x){
+    if(a.includes(x)){
+      return true;
+    }else{
+      return false;
+    };
+};
